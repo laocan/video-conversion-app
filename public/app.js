0dd5cec731dbf6a2e7ba252719f3988b20ac4349
Felipe Dornelas <felipead@gmail.com>
2015-03-09 22:36:37 -0300

    Switched from flow.js to angular-file-upload, which is a superior component  for file uploading.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,39 @@
-var app = angular.module('app', ['flow']);
+angular.module('app', ['angularFileUpload'])
+    .controller("AppController", function ($scope, $http, FileUploader) {
+
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'https://video-conversion-service.herokuapp.com/v1/video-upload',
+            autoUpload: 'true'
+        });
+
+        uploader.onSuccessItem = function (fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+            var inputVideoId = response['id'];
+
+            $scope.showUploadedVideoMessage = true;
+            $scope.uploadedVideoUri = response['uri'];
+            console.info("Created video resource: " + inputVideoId);
+
+            $http.post('https://video-conversion-service.herokuapp.com/v1/video-conversions',
+                {
+                    inputVideoId: inputVideoId,
+                    outputFormat: 'mp4'
+                }
+            ).success(function (data, status, headers, config) {
+                    var videoConversionId = data["id"];
+                    console.info("Created video conversion resource: " + videoConversionId);
+                }).
+                error(function (data, status, headers, config) {
+
+                });
+        };
+
+        console.info('uploader', uploader);
+    })
+    .factory('videoService', function($http) {
+        return {
+            getVideo: function(id) {
+                return $http.get('https://video-conversion-service.herokuapp.com/v1/videos/' + id)
+            }
+        }
+    });
