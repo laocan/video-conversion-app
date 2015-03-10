2000a8edab25281f8dac17f129ddd5d7cc2f68bd
Felipe Dornelas <felipead@gmail.com>
2015-03-10 02:34:52 -0300

    Web application now works as expected. Only issue is the HTLM5 <video> tag that is not working correctly.

--- public/app.js
+++ public/app.js
@@ -1,39 +1,75 @@
-angular.module('app', ['angularFileUpload'])
-    .controller("AppController", function ($scope, $http, FileUploader) {
+"use strict";
+
+angular.module("app", ["angularFileUpload"])
+    .controller("AppController", function ($scope, $log, $http, $interval, FileUploader, service) {
 
         var uploader = $scope.uploader = new FileUploader({
-            url: 'https://video-conversion-service.herokuapp.com/v1/video-upload',
-            autoUpload: 'true'
+            url: "https://video-conversion-service.herokuapp.com/v1/video-upload",
+            autoUpload: "true"
         });
 
-        uploader.onSuccessItem = function (fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-            var inputVideoId = response['id'];
+        uploader.onSuccessItem = function (fileItem, response) {
+            var inputVideoId = response["id"];
 
             $scope.showUploadedVideoMessage = true;
-            $scope.uploadedVideoUri = response['uri'];
-            console.info("Created video resource: " + inputVideoId);
+            $scope.uploadedVideoUri = response["uri"];
 
-            $http.post('https://video-conversion-service.herokuapp.com/v1/video-conversions',
-                {
-                    inputVideoId: inputVideoId,
-                    outputFormat: 'mp4'
-                }
-            ).success(function (data, status, headers, config) {
-                    var videoConversionId = data["id"];
-                    console.info("Created video conversion resource: " + videoConversionId);
-                }).
-                error(function (data, status, headers, config) {
-
+            service.createVideoConversion({
+                inputVideoId: inputVideoId,
+                outputFormat: "mp4"
+            }).then(
+                function (response) {
+                    $scope.showVideoConversion = true;
+                    $scope.videoConversionId = response.data.id;
+                    $scope.videoConversionStatus = response.data.status;
+                    $scope.videoConversionProgress = response.data.progress;
+                    $scope.outputVideoId = response.data.outputVideoId;
+                },
+                function (error) {
+                    $log.error("could not create video conversion.", error);
                 });
-        };
 
-        console.info('uploader', uploader);
+            var promise = $interval(function() {
+                service.getVideoConversion($scope.videoConversionId).then(
+                    function (response) {
+                        $scope.videoConversionStatus = response.data.status;
+                        $scope.videoConversionProgress = response.data.progress;
+                        $scope.outputVideoId = response.data.outputVideoId;
+                        if ($scope.videoConversionStatus == "finished" || $scope.videoConversionStatus == "cancelled" || $scope.videoConversionStatus == "failed") {
+                            $interval.cancel(promise);
+                            if ($scope.videoConversionStatus == "finished") {
+                                service.getVideo($scope.outputVideoId).then(
+                                    function (response) {
+                                        $scope.showConvertedVideo = true;
+                                        $scope.convertedVideoUri = response.data.uri;
+                                    },
+                                    function (error) {
+                                        $log.error("could not get video.", error);
+                                    }
+                                );
+                            }
+                        }
+                        $log.error("loop");
+                    },
+                    function (error) {
+                        $log.error("could not get video conversion.", error);
+                    });
+            }, 1000);
+
+
+
+        }
     })
-    .factory('videoService', function($http) {
+    .factory("service", function ($http) {
         return {
-            getVideo: function(id) {
-                return $http.get('https://video-conversion-service.herokuapp.com/v1/videos/' + id)
+            getVideo: function (id) {
+                return $http.get("https://video-conversion-service.herokuapp.com/v1/videos/" + id)
+            },
+            getVideoConversion: function (id) {
+                return $http.get("https://video-conversion-service.herokuapp.com/v1/video-conversions/" + id)
+            },
+            createVideoConversion: function (payload) {
+                return $http.post("https://video-conversion-service.herokuapp.com/v1/video-conversions", payload)
             }
         }
     });
