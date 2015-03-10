d062e7c7487e24d426f2e240467ff5aee6854856
Felipe Dornelas <felipead@gmail.com>
2015-03-10 12:58:55 -0300

    - Fixed uploader behavior. Now it allows to re-upload files. The progress bar is reset after each upload. - Clean-up javascript a little bit.

--- public/app.js
+++ public/app.js
@@ -4,38 +4,45 @@ angular
 
     .module("app", ["angularFileUpload"])
 
-    .controller("AppController", function ($scope, $log, $http, $interval, FileUploader, service) {
+    .controller("AppController", function ($scope, $log, $http, $interval, service, FileUploader) {
 
         var uploader = $scope.uploader = new FileUploader({
             url: "https://video-conversion-service.herokuapp.com/v1/video-upload",
-            autoUpload: "true"
+            autoUpload: true,
+            removeAfterUpload: true
         });
+
+        FileUploader.FileSelect.prototype.isEmptyAfterSelection = function() {
+            return true;
+        };
+
+        uploader.onBeforeUploadItem = function() {
+            $scope.showUploadProgress = true;
+            $scope.showUploadedVideoMessage = false;
+            $scope.showVideoConversion = false;
+            $scope.showConvertedVideo = false;
+        };
 
         uploader.onSuccessItem = function (fileItem, response) {
             var inputVideoId = response["id"];
             var inputVideoUri = response["uri"];
 
+            $scope.showUploadProgress = false;
             $scope.showUploadedVideoMessage = true;
             $scope.uploadedVideoUri = inputVideoUri;
 
-            service.createVideoConversion({
-                inputVideoId: inputVideoId,
-                outputFormat: "mp4"
-            }).then(
+            service.createVideoConversion({ inputVideoId: inputVideoId, outputFormat: "mp4" }).then(
                 function (response) {
-                    $scope.showVideoConversion = true;
                     $scope.videoConversionId = response.data.id;
-                    $scope.videoConversionStatus = response.data.status;
-                    $scope.videoConversionProgress = response.data.progress;
-                    $scope.outputVideoId = response.data.outputVideoId;
                 },
                 function (error) {
-                    $log.error("could not create video conversion job.", error);
+                   $log.error("could not create video conversion job.", error);
                 });
 
-            var promise = $interval(function() {
+            var promise = $interval(function () {
                 service.getVideoConversion($scope.videoConversionId).then(
                     function (response) {
+                        $scope.showVideoConversion = true;
                         $scope.videoConversionStatus = response.data.status;
                         $scope.videoConversionProgress = response.data.progress;
                         $scope.outputVideoId = response.data.outputVideoId;
@@ -43,23 +50,27 @@ angular
                         var status = $scope.videoConversionStatus;
                         if (status == "finished" || status == "cancelled" || status == "failed") {
                             $interval.cancel(promise);
-                            if (status == "finished") {
-                                service.getVideo($scope.outputVideoId).then(
-                                    function (response) {
-                                        $scope.showConvertedVideo = true;
-                                        $scope.convertedVideoUri = response.data.uri;
-                                    },
-                                    function (error) {
-                                        $log.error("could not get video.", error);
-                                    }
-                                );
-                            }
+                            onVideoConversionFinished(status);
                         }
                     },
                     function (error) {
                         $log.error("could not get video conversion job.", error);
                     });
             }, 1000);
+
+            function onVideoConversionFinished(status) {
+                if (status == "finished") {
+                    service.getVideo($scope.outputVideoId).then(
+                        function (response) {
+                            $scope.showConvertedVideo = true;
+                            $scope.convertedVideoUri = response.data.uri;
+                        },
+                        function (error) {
+                            $log.error("could not get video.", error);
+                        }
+                    );
+                }
+            }
         }
     })
 
